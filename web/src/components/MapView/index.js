63fc9bdad1cf38927c7694ca3c60bcd4500395fc
Sammy Yousif <syousif@utexas.edu>
2017-06-27 01:04:41 -0400

    removed react-mapbox-gl in favor of custom solution

--- web/src/components/MapView/index.js
+++ web/src/components/MapView/index.js
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styles from './styles.css';
-import ReactMapboxGl, { GeoJSONLayer } from 'react-mapbox-gl';
-import { setZoom, symbolLayout, circlePaint } from 'reducers/map';
+import * as MapboxGl from 'mapbox-gl';
+import _ from 'lodash';
+import { setZoom, setLoaded, symbolVisibility, circleSize } from 'reducers/map';
 
-const Map = ReactMapboxGl({
-  accessToken:
-    'pk.eyJ1IjoiaGFtZWVkbyIsImEiOiJHMnhTMDFvIn0.tFZs7sYMghY-xovxRPNNnw',
-});
+MapboxGl.accessToken =
+  'pk.eyJ1IjoiaGFtZWVkbyIsImEiOiJHMnhTMDFvIn0.tFZs7sYMghY-xovxRPNNnw';
 
 const mapStateToProps = state => ({
+  loaded: state.map.loaded,
   zoom: state.map.zoom,
   stopsJSON: state.home.stopsJSON,
-  symbolLayout: symbolLayout(state),
-  circlePaint: circlePaint(state),
+  symbolVisibility: symbolVisibility(state),
+  circleSize: circleSize(state),
 });
 
 const mapDispatchToProps = {
   setZoom,
+  setLoaded,
 };
 
 const symbolPaint = {
   'text-color': '#000',
   'text-halo-width': 1,
   'text-halo-color': '#fff',
+};
+
+const symbolLayout = {
+  'text-field': '{stop_name}',
+  'text-size': 10,
+  'text-offset': [1, 0],
+  'text-anchor': 'left',
+  'text-justify': 'left',
+};
+
+const circlePaint = {
+  'circle-color': '#157AFC',
+  'circle-stroke-color': '#fff',
 };
 
 const circleLayout = { visibility: 'visible' };
@@ -37,46 +51,109 @@ export default class MapView extends Component {
     width: '100%',
   };
 
-  _init = (map, e) => {};
+  componentDidMount() {
+    this._map = new MapboxGl.Map({
+      container: this._mapRef,
+      style: MapView.mapStyle,
+      center: MapView.mapCenter,
+      zoom: this.props.zoom,
+    });
 
-  _renderStops = () => {
-    const stopsJSON = this.props.stopsJSON;
-    if (!stopsJSON) return null;
-    return (
-      <GeoJSONLayer
-        data={stopsJSON}
-        circleLayout={circleLayout}
-        circlePaint={this.props.circlePaint}
-        symbolLayout={this.props.symbolLayout}
-        symbolPaint={symbolPaint}
-        before="road-oneway-arrows-white"
-      />
-    );
+    this._map.on('load', this._onLoad);
+  }
+
+  componentDidUpdate(previous) {
+    const loaded = !previous.loaded && this.props.loaded;
+    if (!this.props.loaded) return;
+    if (loaded || previous.stopsJSON !== this.props.stopsJSON) {
+      this._setStops();
+    }
+    if (previous.symbolVisibility !== this.props.symbolVisibility) {
+      this._updateLabels();
+    }
+    if (previous.circleSize !== this.props.circleSize) {
+      this._updateStops();
+    }
+  }
+
+  _setStops = () => {
+    const stopSource = this._map.getSource('stops');
+    if (!this.props.stopsJSON && stopSource) {
+      this._map.removeSource('stops');
+      this._map.removeLayer('stopCircles');
+      this._map.removeLayer('stopLabels');
+    } else if (this.props.stopsJSON && stopSource) {
+      stopSource.setData(this.props.stopsJSON);
+    } else if (this.props.stopsJSON) {
+      this._map.addSource('stops', {
+        type: 'geojson',
+        data: this.props.stopsJSON,
+      });
+      this._map.addLayer(
+        {
+          id: 'stopCircles',
+          type: 'circle',
+          source: 'stops',
+          paint: {
+            ...circlePaint,
+            ...this.props.circleSize,
+          },
+          layout: circleLayout,
+        },
+        'road-oneway-arrows-white'
+      );
+      this._map.addLayer(
+        {
+          id: 'stopLabels',
+          type: 'symbol',
+          source: 'stops',
+          paint: symbolPaint,
+          layout: {
+            ...symbolLayout,
+            ...this.props.symbolVisibility,
+          },
+        },
+        'road-oneway-arrows-white'
+      );
+      this._map.on('mouseenter', 'stopCircles', () => {
+        this._map.getCanvas().style.cursor = 'pointer';
+      });
+      this._map.on('mouseleave', 'stopCircles', () => {
+        this._map.getCanvas().style.cursor = '';
+      });
+    }
   };
 
-  _onClick = (map, e) => {
-    console.log(e);
+  _updateLabels = () => {
+    const stopLabels = this._map.getLayer('stopLabels');
+    if (!stopLabels) return;
+    _(this.props.symbolVisibility).forEach((val, key) => {
+      this._map.setLayoutProperty('stopLabels', key, val);
+    });
+  };
+
+  _updateStops = () => {
+    const stopCircles = this._map.getLayer('stopCircles');
+    if (!stopCircles) return;
+    _(this.props.circleSize).forEach((val, key) => {
+      this._map.setPaintProperty('stopCircles', key, val);
+    });
+  };
+
+  _onLoad = () => {
+    this.props.setLoaded();
+    this._map.on('zoom', this._onZoom);
   };
 
   _onZoom = (map, e) => {
-    this.props.setZoom([map.getZoom()]);
+    this.props.setZoom([this._map.getZoom()]);
+  };
+
+  _setMapRef = ref => {
+    this._mapRef = ref;
   };
 
   render() {
-    return (
-      <div className={styles.map}>
-        <Map
-          zoom={this.props.zoom}
-          style={MapView.mapStyle}
-          containerStyle={MapView.containerStyle}
-          center={MapView.mapCenter}
-          onStyleLoad={this._init}
-          onClick={this._onClick}
-          onZoom={this._onZoom}
-        >
-          {this._renderStops()}
-        </Map>
-      </div>
-    );
+    return <div className={styles.map} ref={this._setMapRef} />;
   }
 }
--- web/src/reducers/map.js
+++ web/src/reducers/map.js
@@ -4,12 +4,19 @@ import { createSelector } from 'reselect';
 const prefix = 'map/';
 export const map = {
   zoom: `${prefix}zoom`,
+  loaded: `${prefix}loaded`,
 };
 
 export function setZoom(payload) {
   return {
     type: map.zoom,
     payload,
+  };
+}
+
+export function setLoaded() {
+  return {
+    type: map.loaded,
   };
 }
 
@@ -20,51 +27,59 @@ function zoom(state = [11], action = {}) {
   return state;
 }
 
+function loaded(state = false, action = {}) {
+  if (action.type === map.loaded) {
+    return true;
+  }
+  return state;
+}
+
 export default combineReducers({
   zoom,
+  loaded,
 });
 
-export const symbolLayout = createSelector(
-  state => state.map.zoom,
+const visible = {
+  visibility: 'visible',
+};
+
+const hidden = {
+  visibility: 'none',
+};
+
+export const symbolVisibility = createSelector(
+  state => state.map.zoom[0],
   zoom => {
     if (zoom > 15.5) {
-      return {
-        'text-field': '{stop_name}',
-        'text-size': 10,
-        'text-offset': [1, 0],
-        'text-anchor': 'left',
-        'text-justify': 'left',
-      };
+      return visible;
     }
-    return {
-      visibility: 'none',
-    };
+    return hidden;
   }
 );
 
-export const circlePaint = createSelector(
-  state => state.map.zoom,
+const large = {
+  'circle-radius': 5,
+  'circle-stroke-width': 1,
+};
+
+const medium = {
+  'circle-radius': 2,
+  'circle-stroke-width': 1,
+};
+
+const small = {
+  'circle-radius': 1,
+  'circle-stroke-width': 1,
+};
+
+export const circleSize = createSelector(
+  state => state.map.zoom[0],
   zoom => {
     if (zoom > 14) {
-      return {
-        'circle-color': '#157AFC',
-        'circle-radius': 5,
-        'circle-stroke-width': 1,
-        'circle-stroke-color': '#fff',
-      };
+      return large;
     } else if (zoom > 12) {
-      return {
-        'circle-color': '#157AFC',
-        'circle-radius': 2,
-        'circle-stroke-width': 1,
-        'circle-stroke-color': '#fff',
-      };
+      return medium;
     }
-    return {
-      'circle-color': '#157AFC',
-      'circle-radius': 1,
-      'circle-stroke-width': 1,
-      'circle-stroke-color': '#fff',
-    };
+    return small;
   }
 );
